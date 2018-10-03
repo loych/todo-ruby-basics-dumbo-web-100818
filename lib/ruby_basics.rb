8549438091a36544d706c30ce557a5b68c7b5510
IS Choi <insil321@hotmail.com>
2018-10-03 05:26:31 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,40 @@
 def division(num1, num2)
- 
+puts  #{num1 / num2}
 end
+division(num1, num2)
 
 def assign_variable(value)
-  
+  value = "Bob"
+  puts #{value}
 end
+assign_variable(value)
 
 def argue
-  
+  phrase = "I'm right and you are wrong!"
+  puts phrase
+end
+argue
+
+def greeting(greeting, name)
+  greeting = "Hi there, "
+      name = "Bobby!"
+  puts #{greeting, name}
+
 end
 
-def greeting
-
-end
 
 def return_a_value
-
+phrase= "nice"
+puts phrase
 end
+return_a_value
 
 def last_evaluated_value
-
+phrase="expert"
+puts phrase
 end
+last_evaluated_value
 
 def pizza_party
-
+puts "cheese"
 end
